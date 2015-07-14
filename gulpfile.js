50b0fd7027d027c240724ad03ab47cac0df64f86
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-13 21:21:02 -0700

    added more stuff?

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  watch = require('gulp-watch');
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('reload', function() {
+  gulp.src('*.html')
+    .pipe(connect.reload());
+
+  console.log('detected change');
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./*.html', ['reload']);
+  gulp.watch('.css/*.css', ['reload']);
+})
+
+gulp.task('default', ['webserver', 'watch']);
